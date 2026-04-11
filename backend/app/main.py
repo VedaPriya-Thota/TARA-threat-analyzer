7ddf64f9669d36304152f0ef632da02286f3de6a
VedaPriya-Thota <vedapriyathota@gmail.com>
2026-04-11 11:44:58 +0530

    Added Changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,32 +1,3 @@
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-
-# from app.routes import systems, analysis
-
-# app = FastAPI(title="TARA Threat Analyzer")   # ✅ THIS MUST EXIST
-
-# # CORS
-# origins = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# # Routers
-# app.include_router(systems.router)
-# app.include_router(analysis.router)
-
-# @app.get("/")
-# def home():
-#     return {"message": "TARA Threat Analyzer Running"}
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 

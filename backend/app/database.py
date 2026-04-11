7531e7d4481b6cea689dc3c2035221262dff2797
VedaPriya-Thota <vedapriyathota@gmail.com>
2026-04-11 11:41:33 +0530

    Added changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:Uhd%400285@localhost/tara_db"
+DATABASE_URL = "YOUR_DATABASE_URL_HERE"
 
 engine = create_engine(DATABASE_URL)
 
